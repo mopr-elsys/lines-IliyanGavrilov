f8b0c9986bc981acb219c417aa5a59be92d77dba
IliyanGavrilov <85643821+IliyanGavrilov@users.noreply.github.com>
2021-11-10 09:17:26 +0200

    Add files via upload

--- geom.hpp
+++ geom.hpp
@@ -37,7 +37,7 @@ struct Line {
         double x1 = p1.x, y1 = p1.y;
         double x2 = p2.x, y2 = p2.y;
 
-        A = (y2-y1) / (x2 - x1);
+        A = (y2 - y1) / (x2 - x1);
         B = -1;
         C = y1 - x1*((y2 - y1)/ (x2 - x1));
     }
