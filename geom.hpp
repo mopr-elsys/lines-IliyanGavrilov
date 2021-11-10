20c5cdce3912e5d59a3e56375ddecc8d58495f7a
IliyanGavrilov <85643821+IliyanGavrilov@users.noreply.github.com>
2021-11-10 19:06:31 +0200

    Add files via upload

--- geom.hpp
+++ geom.hpp
@@ -87,7 +87,7 @@ struct Line {
     Line perpendicular(const Point& p) {
         double x = p.x, y = p.y;
         double a = -B, b = A, c = -(a*x+b*y);
-        if(c < 0)
+        if(C < 0)
         {
             c = -c;
         }
